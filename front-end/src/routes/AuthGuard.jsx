ec6faac224877831eb68ec0380a89adf2cc2d6cc
PabloNobrega1209 <nobregapmnobrega@gmail.com>
2026-05-30 18:25:08 -0300

    "(30/05) Reorganização e segurança de rotas no front-end"

--- front-end/src/routes/AuthGuard.jsx
+++ front-end/src/routes/AuthGuard.jsx
@@ -6,56 +6,102 @@ import useWaiting from '../ui/useWaiting'
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
-export default function AuthGuard({ children, adminOnly = false }) {
+import { UserLevel } from './routes'
 
-  const { setAuthUser, authUser, setRedirectLocation } = React.useContext(AuthUserContext)
+export default function AuthGuard({ children, userLevel = UserLevel.ANY }) {
+
+  const {
+    setAuthUser,
+    authUser,
+    setRedirectLocation
+  } = React.useContext(AuthUserContext)
+
   const [status, setStatus] = React.useState('IDLE')
 
   const location = useLocation()
+
   const { showWaiting, Waiting } = useWaiting()
+
   const navigate = useNavigate()
 
   async function checkAuthUser() {
+
     if(setStatus) setStatus('PROCESSING')
+
     showWaiting(true)
+
     try {
+
       const authUser = await myfetch.get('/users/me')
+
       setAuthUser(authUser)
+
     }
     catch(error) {
+
       setAuthUser(null)
+
       console.error(error)
+
       navigate('/login', { replace: true })
+
     }
     finally {
+
       showWaiting(false)
+
       setStatus('DONE')
+
     }
+
   }
 
   React.useEffect(() => {
-    // Salva a rota atual para posterior redirecionamento,
-    // caso a rota atual não seja o próprio login
-    if(! location.pathname.includes('login')) setRedirectLocation(location)
+
+    // Salva a rota atual
+    if(! location.pathname.includes('login')) {
+      setRedirectLocation(location)
+    }
 
     checkAuthUser()
+
   }, [location])
 
-  // Enquanto ainda não temos a resposta do back-end para /users/me,
-  // exibimos um componente Waiting
+  // Enquanto carrega
   if(status === 'PROCESSING') return <Waiting />
 
-  if(authUser) {
-    if(adminOnly && authUser.is_admin) return children
-    else if (adminOnly && !(authUser.is_admin)) return (
+  /*
+    Sem usuário autenticado
+  */
+  if(!authUser && userLevel > UserLevel.ANY) {
+
+    console.log({authUser, userLevel})
+
+    return <Navigate to="/login" replace />
+
+  }
+
+  /*
+    Usuário sem permissão admin
+  */
+  if(!(authUser?.is_admin) &&
+    userLevel === UserLevel.ADMIN) {
+
+    return (
       <Box>
         <Typography variant="h2" color="error">
           Acesso negado
         </Typography>
       </Box>
     )
-    else return children
+
   }
-  else return <Navigate to="/login" replace />
-  
+
+  /*
+    Acesso permitido
+  */
+  console.log('AUTHGUARD:', authUser)
+
+  return children
+
 }
